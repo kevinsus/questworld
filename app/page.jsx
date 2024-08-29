fbb6f6e0bd5a3bf7ab3a1cde672f2f08276ab1cc
Kevin Susanto <z5394328@ad.unsw.edu.au>
2024-08-29 13:37:13 +1000

    setting up project initial

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='min-h-screen bg-red-950 flex flex-col items-center space-y-10'>
+    <div className='min-h-screen bg-red-950 flex flex-col items-center space-y-10 px-10'>
       <div className='text-6xl font-bold mt-10 text-white'>
         Quest World
       </div>
@@ -11,6 +11,31 @@ const Home = () => {
           <span>Chapter - 1</span>
           <h3>Sword</h3>
           <span>Chapter - 1</span>
+        </div>
+        <div className='card'>
+          <span>Chapter - 2</span>
+          <h3>Sword</h3>
+          <span>Chapter - 2</span>
+        </div>
+        <div className='card'>
+          <span>Chapter - 3</span>
+          <h3>Sword</h3>
+          <span>Chapter - 3</span>
+        </div>
+        <div className='card'>
+          <span>Chapter - 4</span>
+          <h3>Sword</h3>
+          <span>Chapter - 4</span>
+        </div>
+        <div className='card'>
+          <span>Chapter - 4</span>
+          <h3>Sword</h3>
+          <span>Chapter - 4</span>
+        </div>
+        <div className='card'>
+          <span>Chapter - 4</span>
+          <h3>Sword</h3>
+          <span>Chapter - 4</span>
         </div>
       </div>
     </div>
